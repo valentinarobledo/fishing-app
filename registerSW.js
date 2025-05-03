1ab4f434a3a87d1b94d28b8919c3bb1dccc21dff
valentinarobledo <valenrobledo31@gmail.com>
2025-05-02 23:48:44 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/fisher-app/sw.js', { scope: '/fisher-app/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/fishing-app/sw.js', { scope: '/fishing-app/' })})}
